8f4f8af6efc86fcab966fdc49f0587473cdbc23b
Kathleen Santos <kathleen.santos@csacademy.com.br>
2025-09-15 20:51:34 -0300

    choice: add hostname image in next.config

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
+        hostname: 'encrypted-tbn0.gstatic.com/images',
         port: '',
         pathname: '/**',
       }
